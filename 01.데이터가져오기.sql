fdb50093a3d466953a2ac9a8e877932fc89b1807
ej <ansdmsl@naver.com>
2021-09-30 15:50:33 +0900

    업뎃

---
+++ 01.데이터가져오기.sql
@@ -0,0 +1,13 @@
+--주석 단축기 CTRL +/
+/*
+여러줄 
+주석
+
+*/
+
+--1./ 테이블의 모든 행과 열을 읽어오기 명령문 실행은 CTML + TENTER 
+SELECT * FROM departments;
+
+-- 2. 특정 열만 조회 
+SELECT department_id, department_name
+FROM departments;
